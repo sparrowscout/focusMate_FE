095cf13efef3e1d764c6f35a10c3f7e7592dc50f
김민경 <105103167+sparrowscout@users.noreply.github.com>
2022-11-16 18:27:42 +0900

    [feat] 네비게이션 라우팅 수정

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,5 +1,22 @@
+import { Link, NavLink } from "react-router-dom";
+import styled from "styled-components";
+
 function Navigation() {
-  return <div>navigation</div>;
+  return (
+    <NavigationBox>
+      <div>
+        <Link to={`chart`}>차트</Link>
+      </div>
+      <div>통계</div>
+      <div>스터디 찾기</div>
+      <div>내 스터디</div>
+    </NavigationBox>
+  );
 }
+
+const NavigationBox = styled.div`
+  display: flex;
+  gap: 20px;
+`;
 
 export default Navigation;
--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import Chart from "./Chart";
 const Router = () => {
   return (
     <Routes>
-      <Route path="/Chart" element={<Chart />} />;
+      <Route path="/chart" element={<Chart />} />;
     </Routes>
   );
 };
